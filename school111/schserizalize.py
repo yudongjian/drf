1d5baa5face0ca768bbd8d4b6dc86ac6040c3a81
yudongjian <2516234365@qq.com>
2022-03-01 08:55:55 +0800

    drf many table find

--- school111/schserizalize.py
+++ school111/schserizalize.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Student, Teacher, learn
 from django import views
 
+"""
+    关联查找，是根据表本身的外键去查找，关联其他的表数据
+"""
+
 
 class StudentModelSerialize(serializers.ModelSerializer):
     class Meta:
@@ -12,13 +16,22 @@ class StudentModelSerialize(serializers.ModelSerializer):
 class LearntModelSerialize(serializers.ModelSerializer):
     class Meta:
         model = learn
-        fields = "__all__"
+        fields = ["cname", "sno"]
 
 
 class StudentLearnModelSerialize(serializers.ModelSerializer):
-    learn1 = LearntModelSerialize(many=True)
+    learn1 = LearntModelSerialize(many=True, read_only=True)
+    print(learn1)
     class Meta:
         model = Student
-        fields = ["sno", "sname", "tno"]
+        fields = ["sno", "sname"]
 
+
+class Student2ModelSerizalize(serializers.ModelSerializer):
+    sname = serializers.StringRelatedField(many=True)
+    class Meta:
+        model = learn
+        # fields = ["id", "sname", "cname"]
+        fields = "__all__"
+        depth = 1
 
